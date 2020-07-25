3a6ee15379de08d1b5a2be2142db5c0a885ca4e4
sidhanshu2003 <33799120+sidhanshu2003@users.noreply.github.com>
2020-07-26 00:26:06 +0530

    Add files via upload

---
+++ Batch 6 Python Day 2 Assignment  .py
@@ -0,0 +1,512 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("Hello")
+
+
+# In[2]:
+
+
+print("Hello
+      Python")
+
+
+# In[5]:
+
+
+print("Hello\Python")
+
+
+# In[7]:
+
+
+print("Hello \ 
+Python")
+
+
+# In[8]:
+
+
+print("Hello        Python")
+
+
+# In[1]:
+
+
+print("""(\_/)
+( •,•)
+(")_(")""")
+
+
+# In[2]:
+
+
+"""(\_/)
+( •,•)
+(")_(")
+
+
+# In[3]:
+
+
+""" (\_/)
+( •,•)
+(")_(") """
+
+
+# In[4]:
+
+
+print('Python's world)
+
+
+# In[5]:
+
+
+print('Python's world')
+
+
+# In[6]:
+
+
+print('Python"'"s world')
+
+
+# In[7]:
+
+
+print('Python\'s world')
+
+
+# In[8]:
+
+
+print('Python\tabworld')
+
+
+# In[9]:
+
+
+print('Python\tworld')
+
+
+# In[10]:
+
+
+print('Python\nworld')
+
+
+# In[11]:
+
+
+print('Python\aworld')
+
+
+# In[36]:
+
+
+name = "sudhanshu"
+marks = 90.987
+age = 38
+print ("The name of person is ", name, "marks is ", marks, "age is ",age)
+
+
+# In[39]:
+
+
+print ("The name of person is %10s Marks is %10.2f Age is %5d"%(name,marks,age))
+
+
+# In[41]:
+
+
+print (f"The name of person is {name} Marks is {marks} Age is {age}")
+
+
+# In[42]:
+
+
+10a = 20
+
+
+# In[43]:
+
+
+a10 = 20
+
+
+# In[44]:
+
+
+if = 10
+
+
+# In[45]:
+
+
+ifa = 10
+
+
+# In[46]:
+
+
+_ = 10
+
+
+# In[47]:
+
+
+_a = 10
+
+
+# In[49]:
+
+
+ciso_temp = 10
+
+
+# In[50]:
+
+
+x = y =z = 10
+
+
+# In[51]:
+
+
+x
+
+
+# In[52]:
+
+
+y
+
+
+# In[53]:
+
+
+z
+
+
+# In[58]:
+
+
+id(x)
+
+
+# In[59]:
+
+
+id(y)
+
+
+# In[60]:
+
+
+id(z)
+
+
+# In[61]:
+
+
+del(x)
+
+
+# In[63]:
+
+
+id(y)
+
+
+# In[64]:
+
+
+5**5
+
+
+# In[65]:
+
+
+1+2
+
+
+# In[69]:
+
+
+2-3
+
+
+# In[67]:
+
+
+10/2
+
+
+# In[68]:
+
+
+10%2
+
+
+# In[70]:
+
+
+10%3
+
+
+# In[71]:
+
+
+10/3
+
+
+# In[72]:
+
+
+10//3
+
+
+# In[73]:
+
+
+a= 10
+b=20
+a==b
+
+
+# In[75]:
+
+
+c=10
+a==c
+
+
+# In[76]:
+
+
+c<=a
+
+
+# In[77]:
+
+
+a>=c
+
+
+# In[78]:
+
+
+a>=b
+
+
+# In[79]:
+
+
+a!=b
+
+
+# In[80]:
+
+
+a!=c
+
+
+# In[83]:
+
+
+a= 240
+b = 1
+a | b
+
+
+# In[84]:
+
+
+bin(a)
+
+
+# In[86]:
+
+
+bin(b)
+
+
+# In[87]:
+
+
+a&b
+
+
+# In[88]:
+
+
+a = 1
+b = 20
+a > b
+
+
+# In[89]:
+
+
+a >20 or 10>9
+
+
+# In[90]:
+
+
+a >20 and 10>9
+
+
+# In[91]:
+
+
+10 >9 and 20 > M
+
+
+# In[92]:
+
+
+10 < 9 and 20 > M
+
+
+# In[93]:
+
+
+10 < 9 & 20 > M
+
+
+# In[94]:
+
+
+not 10<9
+
+
+# In[95]:
+
+
+not true
+
+
+# In[96]:
+
+
+not True 
+
+
+# In[97]:
+
+
+Str = "Sudhanshu"
+If 's' in Str
+
+
+# In[98]:
+
+
+Str = "Sudhanshu"
+'s' in Str
+
+
+# In[99]:
+
+
+Str = "Python Programming"
+"g" in Str
+
+
+# In[100]:
+
+
+Str = "Python Programming"
+"x" in Str
+
+
+# In[101]:
+
+
+Str = "Python Programming"
+"g" not in Str
+
+
+# In[105]:
+
+
+Str = "Python Programming"
+"x" not in Str
+
+
+# In[106]:
+
+
+a = 10 
+b = 10 
+a is b 
+
+
+# In[108]:
+
+
+id (a)
+
+
+# In[109]:
+
+
+id (b)
+
+
+# In[110]:
+
+
+a = 1.5
+b = 1.5
+a ==b
+
+
+# In[111]:
+
+
+a is b
+
+
+# In[112]:
+
+
+a = 257
+b = 257
+a is b 
+
+
+# In[113]:
+
+
+a == b 
+
+
+# In[114]:
+
+
+10+10/29*4
+
+
+# In[115]:
+
+
+10/29
+
+
+# In[116]:
+
+
+2**-1
+
+
+# In[ ]:
+
+
+
+
